4a529b1f9744390c461af1eb6d3ee9f00b6093c7
Scott Moak <scott.moak@gmail.com>
2010-10-06 14:44:46 -0700

    Added support for sending udp tunnel messages. Implemented protocol basic support.

--- MumbleClient.py
+++ MumbleClient.py
@@ -5,6 +5,7 @@ import socket
 import platform
 import Messagetypes
 import thread
+from PingThread import PingThread
 
 logging.basicConfig(filename=__name__+".log",level=logging.DEBUG)
 
@@ -15,7 +16,7 @@ except:
   exit
 
 try:
-  from Mumble_pb2 import Authenticate, ChannelRemove, ChannelState, ServerSync, TextMessage, UserRemove, UserState, Version
+  from Mumble_pb2 import Authenticate, ChannelRemove, ChannelState, ServerSync, TextMessage, UserRemove, UserState, Version, UDPTunnel
 except:
   warning+="WARNING: This python program requires the python ssl module (available in python 2.6; standalone version may be at found http://pypi.python.org/pypi/ssl/)\n"
 try:
@@ -72,4 +73,100 @@ class MumbleClient:
       packet=packet[packet:]
     self.sockLock.release()
 
-  def sendUdpTunnelMessge(
+  def sendUdpTunnelMessage(self, data):
+    msgType = MessageTypes.UDPTunnel
+    message = UDPTunnel()
+    message.packet = data
+    self.sendMessage(msgType, message)
+
+  def findChannel(self, channel_id):
+    for c in self.channelList:
+      if c['channel_id'] == channel_id:
+        return c
+    return None
+
+  def findUser(self, session_id):
+    for u in self.userList:
+      if u['session_id'] == session_id:
+        return u
+    return None
+
+  def readFully(self,size):
+    msg=""
+    while len(msg)<size:
+      rcvd=self.socket.recv(size-len(msg))
+      msg += rcvd
+      if len(rcvd)==0:
+        logging.error("Socket died while trying to read")
+        return None
+    return msg
+
+  def handleProtocol(self):
+    v = Version()
+    v.version=self.protocolVersion
+    v.release="mmb 0.0.1-dev"
+    a = Authenticate()
+    a.username=self.username
+    if self.password != None:
+      a.password=self.password
+    a.celt_versions.append(-2147483637)
+    self.sendMessage(MessageTypes.Version, v)
+    self.sendMessage(MessageTypes.Authenticate, a)
+    msg = ""
+    while self.isConnected:
+      msg=self.readFully(6)
+      msgType,length=struct.unpack(headerFormat,msg)
+      msg=self.readFully(length)
+      self.processMessage(msgType,msg)
+
+  def processMessage(self, msgType, message):
+    if msgType == MessageTypes.UDPTunnel or msgType == MessageType.Ping:
+      return
+    if msgType == MessageTypes.ServerSync:
+      ss = ServerSync()
+      ss.ParseFromString(message)
+      self.session=ss.session
+      self.authenticated = True
+      u = self.findUser(self.session)
+      self.currentChannel = u['channel_id']
+      self.pingThread = PingThread(self)
+      self.pingThread.start()
+      us = UserState()
+      us.session=self.session
+      self.sendMessage(MessageTypes.UserState, us)
+    elif msgType == MessageTypes.ChannelSync:
+      cs = ChannelState()
+      cs.ParseFromString(message)
+      c = self.findChannel(cs.channel_id)
+      if c != None:
+        c['name'] = cs.name
+        return
+      self.channelList.append({'channel_id':cs.channel_id,'name':cs.name})
+    elif msgType == MessageTypes.ChannelRemove:
+      cr = ChannelRemove()
+      cr.ParseFromString(message)
+      # to do
+    elif msgType == MessageTypes.UserState:
+      us = UserState()
+      us.ParseFromString(message)
+      u = self.findUser(us.session)
+      if u != None:
+        if us.channel_id != None:
+          u['channel_id'] = us.channel_id
+          if us.session == self.session
+            self.currentChannel = u['channel_id']
+        if us.session == self.session:
+          if us.mute != None:
+            self.canSpeak = !us.mute
+          if us.suppress != None:
+            self.canSpeak = !us.suppress
+        return
+      self.userList.append({'session_id':us.session,'name':us.name,'channel_id':us.channel_id})
+    elif msgType == MessageTypes.UserRemove:
+      pass
+    elif msgType == MessageTypes.TextMessage:
+      pass
+    elif msgType == MessageTypes.CryptSetup:
+      pass
+    else:
+      logging.debug("unhandled message type: " + str(msgType))
