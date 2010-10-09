8ae3a749418ca46907a85d1bd44bd60d720b05d7
Scott Moak <scott.moak@gmail.com>
2010-10-09 07:11:21 -0700

    Implemented more of CryptState wrapper. Still not finished.

--- CryptState.py
+++ CryptState.py
@@ -6,3 +6,12 @@ class CryptState(object):
   
   def __init__(self):
     self.obj = lib.CryptState_new()
+
+  def decrypt(self, source, crypted_length):
+    l=c_uint(crypted_length)
+    
+  def isValid(self):
+    return lib.CryptState_isValid(self.obj) != 0
+
+  def setKey(self, rkey, eiv, div):
+        lib.CryptState_setKey(self.obj, rkey, eiv, div)
