c82c2dd1de5983dd8659082086d7cb368d181e4c
Scott Moak <scott.moak@gmail.com>
2010-11-12 17:22:49 -0800

    Some fixes. Still bad quality though.

--- TestDecoder.py
+++ TestDecoder.py
@@ -5,16 +5,14 @@ gobject.threads_init() # This is very important!
 import gst
 import io
 import threading, thread, signal, logging, time
-from collections import deque
 
 log = logging.getLogger("gstreamer")
 
-class Main:#(threading.Thread):
+class Main(threading.Thread):
     def __init__(self):
-#	threading.Thread.__init__(self)
-	self.raw_pcm = deque()
+	threading.Thread.__init__(self)
 	self.cur_buf = ''
-	caps = gst.caps_from_string("audio/x-raw-int, rate=48000, channels=1, width=16, signed=true")
+	caps = gst.caps_from_string("audio/x-raw-int, rate=48000, channels=1, width=16, depth=16, signed=true")
         self.pipeline = gst.Pipeline("mypipeline")
         self.bus = self.pipeline.get_bus()
         self.bus.add_signal_watch()
@@ -22,47 +20,51 @@ class Main:#(threading.Thread):
 	self.bus.connect("message::eos", self.eos_cb)
         self.bus.connect("message::error", self.error_cb)
 	self.bus.connect("message::warning", self.warning_cb)
-#	self.bus.connect("message::state-changed", self.state_cb, self.pipeline)
 
         self.filesrc = gst.element_factory_make("filesrc", "source")
-        self.pipeline.add(self.filesrc)
         self.filesrc.set_property("location", "Soundtrack_Of_My_Life.mp3")
 
-        self.decode = gst.element_factory_make("decodebin", "decode")
+	try:
+	        self.decode = gst.element_factory_make("decodebin2", "decode")
+	except:
+		self.decode = gst.element_factory_make("decodebin", "decode")
         self.decode.connect("new-decoded-pad", self.OnDynamicPad)
-        self.pipeline.add(self.decode)
+#	self.decode.connect("pad-added", self._pad_added)
 
-        self.filesrc.link(self.decode)
 
         self.convert = gst.element_factory_make("audioconvert", "convert")
-        self.pipeline.add(self.convert)
 
 	self.resample = gst.element_factory_make("audioresample", "resample")
-	self.pipeline.add(self.resample)
-	self.convert.link(self.resample)
+	self.capsfilter = gst.element_factory_make("capsfilter", "caps")
+	self.capsfilter.set_property("caps", caps)
 
         self.sink = gst.element_factory_make("appsink", "sink")
-	self.sink.set_property("caps", caps)
-#	self.sink.set_property("emit-signals", True)
-#	self.sink.connect("new-buffer", self._new_buffer)
-        self.pipeline.add(self.sink)
+	self.sink.set_property('sync', False)
+        # The callback to receive decoded data.
+        self.sink.set_property('emit-signals', True)
+        self.sink.connect("new-buffer", self._new_buffer)
 
-        self.resample.link(self.sink)
 
-#        self.pipeline.set_state(gst.STATE_PLAYING)
+	self.pipeline.add(self.filesrc, self.decode, self.convert, self.resample, self.capsfilter, self.sink)
+	self.filesrc.link(self.decode)
+	gst.element_link_many(self.convert, self.resample, self.capsfilter, self.sink)
+	self.main_loop = gobject.MainLoop()
+	self.f = open('Test.pcm', 'wb')
 
     def run(self):
         log.debug("Starting up")
 	self.pipeline.set_state(gst.STATE_PLAYING)
-#	loop = gobject.MainLoop()
-#	gobject.threads_init()
-#	context = loop.get_context()
-#	while 1:
-#		context.iteration(True)
-#		time.sleep(0.10)
+	self.main_loop.run()
+
+#    def _pad_added(self, e, pad):
+	
+
+    def stop(self):
+	log.debug("stopping...")
+	self.pipeline.set_state(gst.STATE_PAUSED)
+	self.main_loop.quit()
 
     def OnDynamicPad(self, dbin, pad, islast):
-        print "OnDynamicPad Called!"
         pad.link(self.convert.get_pad("sink"))
 
     def warning_cb(self, bus, msg):
@@ -73,11 +75,13 @@ class Main:#(threading.Thread):
 
     def eos_cb(self, bus, msg):
 	print "eos",msg
+	self.f.close()
 
     def _new_buffer(self, sink):
-	print "new_buffer"
+#	print "new_buffer"
 	buf = sink.emit("pull-buffer")
-	self.raw_pcm.append(str(buf))
+	self.f.write(buf)
+	#self.raw_pcm.append(str(buf))
 
     def state_cb(self, bus, msg, pipeline):
 	pass
