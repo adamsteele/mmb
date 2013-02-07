73d0438783f82ed002d03686ad6dac4b9ae782e7
adamsteele <steeleam08@gmail.com>
2013-02-06 19:49:21 -0500

    inital changes

--- main.py
+++ main.py
@@ -10,19 +10,18 @@ import wave
 from PacketDataStream import *
 from collections import deque
 from celt import *
-from pymedia import *
+#from pymedia import *
 from MumbleDecoder import MumbleDecoder
-
+import os
 
 LOG_FILENAME = "main.log"
 
 log = logging.getLogger("main")
 
-#HOST = "jubjubnest.net"
-#PORT = 12345
-HOST = "england"#"localhost"
+HOST = "serverip"
+PASS = "password"
 PORT = 64738
-AUDIO_FILE = "Soundtrack_Of_My_Life.mp3"#"original.wav"
+AUDIO_FILE = "tempfile"
 
 def main():
   # Add the log message handler to the logger
@@ -37,10 +36,11 @@ def main():
   PingThread.log.setLevel(logging.DEBUG)
   log.addHandler(handler)
   log.setLevel(logging.DEBUG)
-  AUDIO_QUALITY = 60000
-  compressedSize=min(AUDIO_QUALITY / (100 * 8), 127)
+  AUDIO_QUALITY = 60000 #VBR-Rate
+  compressedSize=min(AUDIO_QUALITY / (800), 127)
   sample_rate=48000
   frame_size = sample_rate / 100
+#CeltEncoder(sample_rate, frame_size, channels)
   ce = CeltEncoder(sample_rate, frame_size, 1)
   ce.setPredictionRequest(0)
   ce.setVBRRate(AUDIO_QUALITY)
@@ -54,7 +54,8 @@ def main():
  # log.debug("Frames: " + str(nf))
  # log.debug("Compression Type: " + str(comptype))
  # log.debug("Compression Name: " + str(compname))
-  observer=MumbleService.MumbleService(HOST,PORT, 'TestBot', None)
+
+  observer=MumbleService.MumbleService(HOST,PORT, 'TestBot', PASS)
   observer.connect()
   outputQueue = deque()
   eos = False
@@ -66,8 +67,12 @@ def main():
  # pcm_data = str(r.data)
 #  dec = MumbleDecoder(sample_rate, 1)
 #  dec.decode_and_resample(AUDIO_FILE)
-  md = MumbleDecoder(sample_rate, 1)
-  md.decode_and_resample(AUDIO_FILE)
+
+#  md = MumbleDecoder(sample_rate, 1)
+#  md.decode_and_resample(AUDIO_FILE)
+
+  f = open(AUDIO_FILE, 'r')
+
   seq = 0
   buffer_size = (sample_rate / 100) * 2
   framesPerPacket = 6
@@ -75,13 +80,14 @@ def main():
   while True:
     if observer.isServerSynched():
       while not eos:
-        buf = md.read_samples(1)
+        buf = f.read(frame_size*2) #480*2=960
+#        buf = md.read_samples(1) #good but not needed
         #buf = dec.get_data(frame_size)
  #       buf = f.read(frame_size*2)
         #offset = offset + frame_size
         if buf == None or len(buf) == 0:
           eos = True
-#	  f.close()
+	  f.close()
           continue
         compressed = ce.encode(buf, compressedSize)
         outputQueue.append(compressed)
@@ -109,7 +115,7 @@ def main():
           data.extend(pds.getDataBlock(size))
           observer.sendUdpMessage("".join(data))
           time.sleep(0.01 * framesPerPacket)
-    time.sleep(10)
+#    time.sleep(10)
 
 
 if __name__ == '__main__':
